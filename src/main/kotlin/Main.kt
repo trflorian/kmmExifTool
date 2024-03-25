154976d71fb4f98db029e2d66c2bcfd50d7d89e2
trflorian <florian.trautweiler@hispeed.ch>
2024-03-25 16:53:40 +0100

    use dark theme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,52 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Orange80,
+    secondary = DarkOrange,
+    surfaceVariant = DarkOrange,
+    tertiary = Pink80,
+    onSecondary = Color.White
+)
 
 @Composable
 @Preview
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+    MaterialTheme(
+        colorScheme = DarkColorScheme
+    ) {
+        Surface(
+            Modifier.fillMaxSize()
+        ) {
+            Button(
+                onClick = {
+                    text = "Hello, Desktop!"
+                }, modifier = Modifier.wrapContentSize()
+            ) {
+                Text(text)
+            }
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Exif Tool",
+    ) {
         App()
     }
 }
