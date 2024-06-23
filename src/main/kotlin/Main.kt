b756b07c20ad93fea3c444fd564e5108aa4cc439
trflorian <florian.trautweiler@hispeed.ch>
2024-06-23 10:18:03 +0200

    use standard dark theme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,17 +8,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Orange80,
-    secondary = DarkOrange,
-    surfaceVariant = DarkOrange,
-    tertiary = Pink80,
-    onSecondary = Color.White
-)
 
 @Composable
 @Preview
